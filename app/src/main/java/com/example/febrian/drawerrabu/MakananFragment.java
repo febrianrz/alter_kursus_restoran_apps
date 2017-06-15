9f8b408a79ac029610e7c058716b0730b6cf9050
Febrian Reza <febrianrz@gmail.com>
2017-06-15 21:03:52 +0700

    Layouting recyler view makanan default

--- app/src/main/java/com/example/febrian/drawerrabu/MakananFragment.java
+++ app/src/main/java/com/example/febrian/drawerrabu/MakananFragment.java
@@ -3,9 +3,15 @@ package com.example.febrian.drawerrabu;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.febrian.drawerrabu.Adapter.RecylerMakananAdapter;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,7 +19,10 @@ import android.view.ViewGroup;
  */
 public class MakananFragment extends Fragment {
 
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recylerAdapter;
+    private RecyclerView.LayoutManager recylerLayoutManager;
+    private ArrayList<String> dataSet;
     public MakananFragment() {
         // Required empty public constructor
     }
@@ -23,7 +32,39 @@ public class MakananFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_makanan, container, false);
+        dataSet = new ArrayList<>();
+        init();
+        View rootView = inflater.inflate(R.layout.fragment_makanan, container, false);
+        //deklarasikan recylerview
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyler_makanan);
+
+        //buat ukurannya tetap
+        recyclerView.setHasFixedSize(true);
+
+        //gunakan linear layout manager
+        recylerLayoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(recylerLayoutManager);
+
+        //setting adapter
+        recylerAdapter = new RecylerMakananAdapter(dataSet);
+        recyclerView.setAdapter(recylerAdapter);
+        return rootView;
     }
 
+    private void init(){
+        dataSet.add("Tester");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+        dataSet.add("Lagi");
+    }
 }
